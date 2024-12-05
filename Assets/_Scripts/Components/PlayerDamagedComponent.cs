1e1d6bcf1b8937c228d11b99166a7f60cc79cec3
xanorade <eyupcanerkal@gmail.com>
2024-12-06 02:10:42 +0300

    we safe

---
+++ Assets/_Scripts/Components/PlayerDamagedComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class PlayerDamagedComponent : IComponent
+{
+    //dotnet Jenny/Jenny.Generator.Cli.dll server
+}
---
+++ Assets/_Scripts/Components/PlayerHealthComponent.cs
@@ -0,0 +1,6 @@
+using Entitas;
+
+public class PlayerHealthComponent : IComponent
+{
+    public float Health;
+}
